00405fa6415a408d92f521e8f30625a5fe7831dd
Q1139821 <david_leon_gonzalez@carrefour.com>
2019-01-11 12:46:12 +0100

    Fix data.sqñ

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
 insert into book
-values(1, 'Titulo1', 'Autor1', 'Editorial1', '150', 'Epica', '2015');
+values(1, 'Titulo1', 'Autor1', 'Editorial1', '151', 'Epica', '2015');
 
 insert into book
-values(2, 'Titulo2', 'Autor2', 'Editorial2', '151', 'Comedia', '2016');
+values(2, 'Titulo2', 'Autor2', 'Editorial2', '152', 'Comedia', '2016');
 
 insert into book
-values(3, 'Titulo3', 'Autor3', 'Editorial3', '152', 'Terror', '2016');
+values(3, 'Titulo3', 'Autor3', 'Editorial3', '153', 'Terror', '2016');
 
 insert into book
-values(4, 'Titulo4', 'Autor1', 'Editorial1', '153', 'Lirica', '2018');
+values(4, 'Titulo4', 'Autor1', 'Editorial1', '154', 'Lirica', '2018');
 
 insert into book
-values(5, 'Titulo4', 'Autor1', 'Editorial1', '153', 'Lirica', '2018');
+values(5, 'Titulo5', 'Autor1', 'Editorial1', '155', 'Comedia', '2018');
 
 insert into book
-values(6, 'Titulo6', 'Autor5', 'Editorial2', '154', 'Policial', '2017');
+values(6, 'Titulo6', 'Autor5', 'Editorial2', '156', 'Policial', '2017');
